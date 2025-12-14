42e3b61ccd258171251599cb5e76819732c7c397
VeronicaDiazTaylor <151398472+VeronicaDiazTaylor@users.noreply.github.com>
2025-12-14 11:38:13 +0900

    ShockWaver実装

--- scripts/weapon/bmc/shockWaver.ts
+++ scripts/weapon/bmc/shockWaver.ts
@@ -35,7 +35,7 @@ class ShockWaver extends Weapon {
     player.setDynamicProperty(this.canSwingKey, true);
 
     // ビヘイビアーで定義したスライムのエレメントを召喚 (as anyでIDEの構文エラーを回避)
-    const slimeElement = player.dimension.spawnEntity('bmc:shock_waver_element' as any, player.location);
+    const slimeElement = player.dimension.spawnEntity('bmc:shock_waver_element' as any, player.getHeadLocation());
 
     // スライムのエレメントに付けるプロパティをここで設定
     slimeElement.setDynamicProperty(this.summonerKey, player.id);
@@ -43,9 +43,12 @@ class ShockWaver extends Weapon {
     // 速度の付与
     const velocity = Vector3.fromBDS(player.getViewDirection()).normalize();
     velocity.x *= 1.2;
-    velocity.y = -0.08;
+    velocity.y = -0.06;
     velocity.z *= 1.2;
     slimeElement.applyImpulse(velocity);
+
+    // 効果音
+    player.dimension.playSound('mob.vex.charge', player.location);
 
     return { duration: ShockWaver.DURATION, cooldown: ShockWaver.COOLDOWN };
   }
@@ -64,16 +67,16 @@ class ShockWaver extends Weapon {
 
     // 速度の付与
     const velocity = Vector3.fromBDS(player.getViewDirection()).normalize();
-    velocity.x *= 1.2;
-    velocity.y = 0.2;
-    velocity.z *= 1.2;
+    velocity.x *= 1.75;
+    velocity.y = 1;
+    velocity.z *= 1.75;
     slime.applyImpulse(velocity);
 
     // スイングデータを初期化
     player.setDynamicProperty(this.canSwingKey, false);
   }
 
-  override onTick(player: Player, isActivated: boolean) {
+  override onTick(player: Player, isWeaponInHand: boolean, isActivated: boolean) {
     // 効果継続中じゃなければ処理しない
     if (!isActivated) return;
 
@@ -105,23 +108,26 @@ class ShockWaver extends Weapon {
     });
 
     // スライムがいなければ処理を終了させる
-    if (!slime || slime.isValid) return;
+    if (!slime || !slime.isValid) return;
 
     // y < -100で消滅
     if (slime.location.y < -100) slime.remove();
-
+    
     for (const p of slime.dimension.getPlayers()) {
 
-      // 衝突しているかどうか
-      if (this.isIntersects(slime.getAABB(), p.getAABB())) {
+      // 距離判断による衝突判定
+      if (this.isIntersects(slime.getAABB(), player.getAABB())) {
 
         // 吹き飛ばし
         p.clearVelocity();
         const velocity = Vector3.fromBDS(p.location).subtractVector(slime.location).normalize();
-        velocity.x *= 1.8;
-        velocity.y = 0.6;
-        velocity.z *= 1.8;
+        velocity.x *= 2.25;
+        velocity.y = 0.95;
+        velocity.z *= 2.25;
         p.applyImpulse(velocity);
+
+        // 効果音
+        player.dimension.playSound('item.trident.riptide_2', player.location);
 
         if (slime.getDynamicProperty(this.summonerKey) != p.id) {
           // 鈍足の付与
@@ -132,12 +138,20 @@ class ShockWaver extends Weapon {
   }
 
   override onEnd(player: Player): void {
-    // 残留しているエレメント及びスライムをデスポーンさせる
+    // 残留しているエレメント及びスライムを取得
     for (const entity of player.dimension.getEntities()) {
       if (entity.typeId === 'bmc:shock_waver_slime' || entity.typeId === 'bmc:shock_waver_element') {
+
+        // 座標上で起こる効果音
+        entity.dimension.playSound('mob.vex.hurt', entity.location);
+
+        // デスポーンさせる
         entity.remove();
       }
     }
+
+    // プレイヤーに送る効果切れ効果音
+    player.playSound('mob.vex.hurt');
   }
 
   /**
@@ -147,6 +161,9 @@ class ShockWaver extends Weapon {
    * @returns {boolean} 
    */
   private isIntersects(a: AABB, b: AABB): boolean {
+    a.extent.x += 1.75;
+    a.extent.y += 1.75;
+    a.extent.z += 1.75;
     return (
       a.center.x - a.extent.x <= b.center.x + b.extent.x && 
       a.center.x + a.extent.x >= b.center.x - b.extent.x &&
