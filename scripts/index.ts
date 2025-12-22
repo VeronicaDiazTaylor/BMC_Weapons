7c0ffddd8eb9cd7f78f1c67c15235d376a3269d2
veronica <151398472+VeronicaDiazTaylor@users.noreply.github.com>
2025-12-22 20:21:52 +0900

    無駄なコメント行の削除

--- scripts/index.ts
+++ scripts/index.ts
@@ -14,22 +14,6 @@
  * ScriptAPIのビヘイビアー開発支援フレームワーク「Keystone」を用いているため、TypeScriptで書かれています。
  * 
  */
-/**
- *           _ _           _              _            
- * __   ____| (_) __ _ ___| |_ __ _ _   _| | ___  _ __ 
- * \ \ / / _` | |/ _` |_  / __/ _` | | | | |/ _ \| '__|
- *  \ V / (_| | | (_| |/ /| || (_| | |_| | | (_) | |   
- *   \_/ \__,_|_|\__,_/___|\__\__,_|\__, |_|\___/|_|   
- *                                  |___/                
- * 
- * @author vdiaztaylor
- * @website https://github.com/VeronicaDiazTaylor
- * 
- * NOTE:
- * このプログラムは非公式サーバーソフトウェアPocketMine-MPで稼働していたBowyersMCをScriptAPIに移植したものです。
- * ScriptAPIのビヘイビアー開発支援フレームワーク「Keystone」を用いているため、TypeScriptで書かれています。
- * 
- */
 import './listener';
 
 import './entity/bmc/shockWaverElement';
